52900b75fd0da8c58041bc9b73705a6942f20321
Alejandro <lopez.gutierrez.a07@gmail.com>
2021-06-24 21:20:43 -0400

    anualr

--- src/pages/Listado.vue
+++ src/pages/Listado.vue
@@ -19,97 +19,110 @@
       title="Listado"
       :data="data"
       :columns="columns"
-      row-key="name"
     >
     <template v-slot:body-cell-opcion="props">
             <q-td :props="props">
-              <q-btn dense round flat color="yellow" @click="editRow(props)" icon="edit"></q-btn>
-              <q-btn dense round flat color="red" @click="deleteRow(props)" icon="delete"></q-btn>
+              <q-btn dense round flat color="green" @click="editRow(props)" icon="receipt_list"></q-btn>
             </q-td>
           </template>
 
     </q-table>
+    
     <q-dialog v-model="dialog_mod">
       <q-card>
         <q-card-section class="bg-green-14 text-white">
-          <div class="text-h6">Modificar</div>
+          <div class="text-h6">Datos de Venta</div>
         </q-card-section>
         <q-card-section class="q-pt-xs">
           <q-form
-            @submit="onMod"
+            @submit="onAnular"
             class="q-gutter-md"
           >
+          <div class="row">
             <q-input
-              filled
-              v-model="dato2.nombre"
+              v-model="dato2.id"
               type="text"
-              label="Nombre"
-              hint="Ingresar nombre"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Por favor ingresa datos']"
+              label="Id Venta"
+              readonly
             />
             <q-input
-              filled
-              v-model="dato2.descripcion"
+              v-model="dato2.nrocomprobante"
               type="text"
-              label="Descripcion"
-              hint="Ingresar descripcion"
-              lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Por favor ingresa datos']"
+              label="Nro Comprobante"
+              readonly
             />
             <q-input
-              filled
-              v-model="dato2.precio"
-              label="Precio"
-              type="number"
-              hint="Ingresar precio"
-              lazy-rules
-              :rules="[ val => val>0 && val < 500 || 'Por favor Valor']"
-            />
-            <q-select
-              v-model="dato2.rubro_id"
-              label="Rubro"
-              :options="options"
-              emit-value
-            />
-
-<!--            <q-input-->
-<!--              filled-->
-<!--              type="text"-->
-<!--              v-model="dato2.color"-->
-<!--              label="Ingresar color"-->
-<!--              lazy-rules-->
-<!--              :rules="[-->
-<!--              val => val && val.length > 0 || 'Por favor ingresa datos',-->
-<!--              // val => val > 0 && val < 100 || 'Please type a real age'-->
-<!--              ]"-->
-<!--            />-->
-
-            <q-input
-              filled
+              v-model="dato2.total"
               type="text"
-              v-model="dato2.imagen"
-              label="Ingresar imagen"
-              lazy-rules
-              :rules="[
-              val =>  val.length > 0 || 'Por favor ingresa datos',
-              // val => val > 0 && val < 100 || 'Please type a real age'
-              ]"
+              label="Total"
+              readonly
             />
+          </div>
+          <div class="row">
+            <q-input
+              v-model="dato2.fecha"
+              type="text"
+              label="Fecha Venta"
+              readonly
+            />
+            <q-input
+              v-model="dato2.client['nombrerazon']"
+              type="text"
+              label="Cliente"
+              readonly
+            />
+          </div>
+          <div class="row">
+            <q-input
+              v-model="dato2.user['name']"
+              type="text"
+              label="Vendedor"
+              readonly
+            />
+            <q-input
+              v-model="dato2.codigocontrol"
+              type="text"
+              label="Codigo Control"
+              readonly
+            />
+            <q-input
+              v-model="dato2.tipo"
+              type="text"
+              label="Tipo"
+              readonly
+            />
+          </div>
+            <h6>PRODUCTOS</h6>
+                <q-table
+                title="Detalle"
+                :data="dato2.details"
+                :columns="rows"
+                >
+                 <template v-slot:body="props">
+                    <q-tr :props="props">
+                    <q-td key="nombreproducto" :props="props">
+                        {{ props.row.nombreproducto }}
+                    </q-td>
+                
+                    <q-td key="cantidad" :props="props">
+                        {{ props.row.cantidad }}
+                    </q-td>
+                    <q-td key="precio" :props="props">
+                        {{ props.row.precio }}
+                    </q-td>
+                    <q-td key="subtotal" :props="props">
+                        {{ props.row.subtotal }}
+                    </q-td>
+                    </q-tr>
+                </template>
 
-<!--            <q-toggle v-model="accept" label="I accept the license and terms" />-->
-
+                </q-table>
             <div>
-              <q-btn label="Modificar" type="submit" color="positive" icon="add_circle"/>
-<!--              <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />-->
-<!--              <q-card-actions align="right">-->
+                <q-btn label="Anular" type="submit" color="red" icon="undo"/>
                 <q-btn  label="Cancelar" icon="delete" color="negative" v-close-popup />
-<!--              </q-card-actions>-->
             </div>
           </q-form>
         </q-card-section>
-
-
       </q-card>
     </q-dialog>
 
@@ -138,7 +151,7 @@ export default {
       alert: false,
       dialog_mod:false,
       dialog_del:false,
-      dato2:{},
+      dato2:{client:[],user:[],details:[]},
       name: null,
       age: null,
       accept: false,
@@ -148,7 +161,7 @@ export default {
       props:[],
       columns: [
         {
-          name: 'sales.id',
+          name: 'sales',
           required: true,
           label: 'id',
           align: 'left',
@@ -165,12 +178,19 @@ export default {
         { name: 'estado', label: 'estado', field: 'estado', sortable: true },
         { name: 'opcion', label: 'Opcion', field:'action',  sortable: false },
       ],
-      data: [
-        // {
-        //   nombre: 'Frozen Yogurt',
-        //   color: 'red',
-        //   imagen:'1.png'
-        // }
+      data: [],
+      rows:[
+        {
+          name: 'nombreproducto',
+          label: 'Producto',
+          align: 'left',
+          field: 'nombreproducto',
+          // format: val => `${val}`,
+        },  
+        { name: 'cantidad', align: 'center', label: 'cantidad', field: 'cantidad' },
+        { name: 'precio', label: 'precio', field: 'precio'},
+        { name: 'subtotal', label: 'subtotal', field: 'subtotal' },
+
       ]
     }
   },
@@ -178,22 +198,12 @@ export default {
     
   },
   methods:{
-    misdatos(){
-      this.$q.loading.show();
-      this.$axios.get(process.env.URL+'/product').then(res=>{
-        // console.log(res.data)
-        this.data=res.data;
-        this.$q.loading.hide();
-      })
-    },
     buscar(){
         this.$axios.post(process.env.URL+'/listabuscar/'+this.fecha).then(res=>{
             console.log(res.data);
             this.data=res.data;
-            // res.data.forEach(row => {
-            //     this.options.push({label:row.nombre,value:row.id});
-            // });
-        })
+        });
+
     },
     editRow(producto){
         console.log(producto.row);
@@ -207,57 +217,36 @@ export default {
 
     },
 
-    onSubmit () {
-      this.$q.loading.show();
-
-      this.$axios.post(process.env.URL+'/product', this.dato).then(res=>{
-        console.log(res.data);
-        this.$q.notify({
-          color: 'green-4',
-          textColor: 'white',
-          icon: 'cloud_done',
-          message: 'Creado correctamente'
-        });
-        this.alert=false;
-        this.misdatos();
-      })
-
-
-    },
-    onMod(){
-        this.$q.loading.show();
-        this.$axios.put(process.env.URL+'/product/'+this.dato2.id,this.dato2).then(res=>{
+    prompt () {
+        $q.dialog({
+          title: 'Anular Venta',
+          message: 'Cual es el motivo? (Minimum 3 characters)',
+          prompt: {
+            model: '',
+            isValid: val => val.length > 2, // << here is the magic
+            type: 'text' // optional
+          },
+          cancel: true,
+          persistent: true
+        }).onOk(data => {
+          // console.log('>>>> OK, received', data)
+        })
+      },
+    onAnular(){
+        this.$axios.post(process.env.URL+'/anular/'+this.dato2.id).then(res=>{
          this.$q.notify({
           color: 'green-4',
           textColor: 'white',
           icon: 'cloud_done',
-          message: 'Modificado correctamente'
+          message: 'Anulado correctamente'
         });
         this.dialog_mod=false;
-        this.misdatos();})
+        this.buscar();})
     },
     onDel(){
-        this.$q.loading.show();
-        this.$axios.delete(process.env.URL+'/product/'+this.dato2.id).then(res=>{
-         this.$q.notify({
-          color: 'green-4',
-          textColor: 'white',
-          icon: 'cloud_done',
-          message: 'Eliminado correctamente'
-        });
-        this.dialog_del=false;
-        this.misdatos();})
+
     },
 
-
-
-    onReset () {
-      this.dato.nombre = null;
-      this.dato.precio = 0.00;
-      this.dato.rubro_id=1;
-      this.dato.color = null;
-      this.dato.imagen=null;
-    }
   }
 }
 </script>
